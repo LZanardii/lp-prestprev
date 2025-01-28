afb7b9f9524e9fc0153805e219d7cbfd945a58c5
LZanardii <leonardo.szanardi@gmail.com>
2025-01-27 22:29:01 -0300

    feat: resposive first section + wpp redirect

--- src/components/FirstSection/index.tsx
+++ src/components/FirstSection/index.tsx
@@ -1,16 +1,45 @@
 
+import { Button, Form } from "react-bootstrap";
 import Header from "../Header"
 import "./style.css"
+import { useState } from "react";
 
 const FirstSection = () => {
+
+  const [inputPergunta, setInputPergunta] = useState<string>("");
+
+  const direcionaPerguntaParaWhatsapp = () => {
+    // Codifica a pergunta para ser utilizada na URL
+    const encodedPergunta = encodeURIComponent(inputPergunta);
+    const url = `https://wa.me/send/?phone=555185816550&text=${encodedPergunta}`;
+    
+    // Abre a URL em uma nova aba
+    window.open(url, "_blank", "noreferrer");
+  };
+
   return (
     <div className='fistSection'>
-      <div className="text">
+      <div className="content">
         <Header/>
-
+        <div className="titleContent">
+          <div className="title">
+            <h1>
+              A garantia dos seus benefícios é <br/>  um direito seu como cidadão!
+            </h1>
+            <h6>
+              A PrestPrev é o escritório especializado em processos previdenciários <br/> que preza facilitar a sua busca pelos seus direitos como cidadão
+            </h6>
+            <Form>
+              <Form.Control type="text" placeholder="Como podemos te ajudar?" onChange={e => setInputPergunta(e.target.value)} />
+              <Button variant="primary" type="submit" onClick={() => direcionaPerguntaParaWhatsapp()}>
+                Enviar
+              </Button>
+            </Form>
+          </div>
+        </div>
+       
       </div>
-      <img src="src/assets/lp-prestprev-home.png" alt="Girl in a jacket"/>
-      
+      <img className="backgroundImage" src="src/assets/lp-prestprev-home.png" alt="carteiras de trabalho na mesa"/>
     </div>
   )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,14 +4,21 @@ import "./style.css"
 
 const Header = () => {
   return (
-    <div className="card">
-      <div className="header">
-        <img src="src/assets/prestPrevLogoComFundo.png" width='5px' height='70px'/>
+    <div className="header">
+      <div className="headerBox">
+        <img src="src/assets/prestPrevLogoComFundo.png" width='5px' height='70px' alt="logo prestprev"/>
         <span>Serviços</span>
         <span>Sobre</span>
         <span>Depoimentos</span>
         <span>Contato</span>
-        <Button variant="success"  size="sm"><SiWhatsapp/>&nbsp;&nbsp;Entrar em contato!&nbsp;</Button>
+        <Button 
+          variant="success"  
+          size="sm"
+          href="https://wa.me/send/?phone=555185816550&text=Olá+Matheus!%20Pode+me+ajudar?"
+          target="_blank"
+        >
+          <SiWhatsapp/>&nbsp;&nbsp;Entrar em contato!&nbsp;
+        </Button>
       </div>
     </div>
   )
