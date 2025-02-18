e58f0e6a23133b196af02c64078470d53f75a7d3
LZanardii <leonardo.szanardi@gmail.com>
2025-02-17 22:34:28 -0300

    fix: build

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from 'src/pages/Home'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { APIProvider } from "@vis.gl/react-google-maps";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <APIProvider apiKey={"AIzaSyCPC4wTuAmLszYGEcpgkwHjz8uvrYlmx08"}>
-      <Home />
-    </APIProvider>
+    <Home />
   </React.StrictMode>,
 )
